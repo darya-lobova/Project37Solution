e5f71bb6a12f92caeb57f629f02584cfaaba2709
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-05 20:22:03 +0300

    task 01 final

--- Task 01/logic.cpp
+++ Task 01/logic.cpp
@@ -2,14 +2,24 @@
 
 bool are_all_digits_diffrent(long long number) {
 
-	int digit;
-	int all_digits = 0;
+	int digit1;
+	int digit2;
+	int remaining_digits;
 
 	while (number > 0) {
-		digit = number % 10;
+		digit1 = number % 10;
+		remaining_digits = number / 10;
+		
+		while (remaining_digits > 0) {
+			digit2 = remaining_digits % 10;
+			if (digit1 == digit2) {
+				return false;
+				break;
+			}
+			remaining_digits /= 10;
+		}
 		number /= 10;
-
 	}
 
-	return false;
+	return true;
 }
